13071bceb8969d115fb42996640bcd1503d634b2
Sapna Jindal <sapna.jindal@practo.com>
2016-08-11 18:56:47 +0000

    Revert "Revert "uploading to s3 using paperclip""
    This reverts commit 9685dabbd58d9faad2f6682cff63c61cad150793.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@
 #
 # Table name: users
 #
+#  avatar_content_type    :string(255)
+#  avatar_file_name       :string(255)
+#  avatar_file_size       :integer
+#  avatar_updated_at      :datetime
 #  created_at             :datetime         not null
 #  current_sign_in_at     :datetime
 #  current_sign_in_ip     :string(255)
@@ -25,6 +29,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
